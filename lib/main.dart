6dd8122b7695c730b16645631199ecf6986dd11f
dong- <dong-jane@hotmail.com>
2020-03-08 04:57:19 -0700

    full folder

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,30 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'api.dart';
 
-void main() => runApp(MyApp());
 class BaseLayout extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/bulb.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: new Image.asset("assets/images/bulb.jpg"),
-      ),
+    return new Scaffold(
+        body: new Container(
+            child: new Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                child: [
+                  Text(
+                    "Lorem ipsum",
+                    style: bold24Roboto,
+                  ),
+                ]
+            )
+        )
     );
   }
 }
 
+
+
+//void main() => runApp(MyApp());
+//
+//
 //class MyApp extends StatelessWidget {
 //  @override
 //  Widget build(BuildContext context) {
