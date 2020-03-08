b388e0db1fa90f8000b9a8b30903b719d5af4889
dong- <dong-jane@hotmail.com>
2020-03-08 11:12:38 -0700

    final v2

--- lib/Paths.dart
+++ lib/Paths.dart
@@ -31,9 +31,6 @@ class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
       body: Center(
         child: RaisedButton(
           onPressed: () {
